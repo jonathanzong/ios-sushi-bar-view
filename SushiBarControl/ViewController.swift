34ce254ffc2c863487d85acfaac496075456a8a8
Jonathan Zong <jonathan@jonathanzong.com>
2015-09-17 22:02:03 -0400

    Initial Commit

---
+++ SushiBarControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SushiBarControl
+//
+//  Created by Jonathan Zong on 9/17/15.
+//  Copyright (c) 2015 Jonathan Zong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
